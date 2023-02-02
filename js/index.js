cdba64322f4c1694cf5656381240547ec705add6
girish sharma <girish@sozeno.com>
2023-02-02 15:57:47 +0530

    update review section

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ window.onload = function() {
         slideIndex++;
         if (slideIndex > slides.length) { slideIndex = 1 }
         slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
+        setTimeout(showSlides, 4000); // Change image every 2 seconds
     }
 
     document.getElementById("prevBtn").addEventListener("click", function() {
